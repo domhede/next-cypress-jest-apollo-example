e05216ae9bd5baba636c05a43024f4a235c6e49e
Dom Hede <domhede@gmail.com>
2021-03-17 09:42:18 +1100

    Add config for jest coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  setupFilesAfterEnv: ['./jest.setup.js'],
+  setupFilesAfterEnv: [
+    './jest.setup.js',
+    '@testing-library/jest-dom/extend-expect',
+  ],
+  testMatch: ['<rootDir>/**/?(*.)+(test).[jt]s?(x)'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  collectCoverage: true,
+  coverageDirectory: './coverage',
 };
