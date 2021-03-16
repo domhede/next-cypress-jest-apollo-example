071d9d4e5c52545d7c446c0cd484ef9220c7cfa7
Dom Hede <domhede@gmail.com>
2021-03-17 08:35:55 +1100

    Add example spec for Hello World

---
+++ cypress/integration/hello-world.spec.js
@@ -0,0 +1,8 @@
+import '@testing-library/cypress/add-commands';
+
+describe('Hello world', () => {
+  it('Should visit home page', () => {
+    cy.visit('/');
+    cy.findAllByText('Hello Jerry').should('exist');
+  });
+});
